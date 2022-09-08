0e822aede4fa43c81dee0b46a05abba6500aa5d1
Ashraful <md.ashrafula.alam@ericsson.com>
2022-09-08 16:31:00 +0200

    added lvm to gitops

--- deploy-sno.sh
+++ deploy-sno.sh
@@ -5,10 +5,10 @@ SCRIPTDIR=$(dirname $(realpath "${BASH_SOURCE[0]}"))
 
 oc create -k $SCRIPTDIR/ztp/gitops-subscriptions/argocd/deployment/
 
-oc create -f $SCRIPTDIR/ztp/gitops-subscriptions/argocd/policy/lvm_policy.yaml -n lvm-policy-ns
+#oc create -f $SCRIPTDIR/ztp/gitops-subscriptions/argocd/policy/lvm_policy.yaml -n lvm-policy-ns
 
-oc create -f $SCRIPTDIR/ztp/gitops-subscriptions/argocd/policy/localvolume-namespace.yaml
+#oc create -f $SCRIPTDIR/ztp/gitops-subscriptions/argocd/policy/localvolume-namespace.yaml
 
-oc create -f $SCRIPTDIR/ztp/gitops-subscriptions/argocd/policy/localstorage-localvolume-policy.yaml -n localvolume-policy-ns
+#oc create -f $SCRIPTDIR/ztp/gitops-subscriptions/argocd/policy/localstorage-localvolume-policy.yaml -n localvolume-policy-ns
 
-oc patch storageclass odf-lvm-vg1 -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+#oc patch storageclass odf-lvm-vg1 -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
